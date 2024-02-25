16edcd2ac2ee13d40b008c4adeeb6bc5137dfb65
davidscottconrad <david.scott.conrad@gmail.com>
2024-02-24 22:49:45 -0500

    adding axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import axios from "axios";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const apiURL = process.env.VUE_APP_API_URL;
+const axiosInstance = axios.create({ baseURL: apiURL });
+app.config.globalProperties.$axios = axiosInstance;
+
+app.mount("#app");
