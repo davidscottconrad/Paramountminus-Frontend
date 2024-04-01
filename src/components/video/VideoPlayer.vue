ee74f3303668a198440c01832c8b5bc01a2f8053
davidscottconrad <david.scott.conrad@gmail.com>
2024-04-01 14:57:21 -0400

    Added the other sstuff

---
+++ src/components/video/VideoPlayer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+
+<script>
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+        console.log("Video player is ready");
+      });
+    });
+  },
+  beforeUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
+
+<style scoped>
+/* Add any custom styles for the video player here */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+const apiURL = process.env.VUE_APP_API_URL;
+const axiosInstance = axios.create({ baseURL: apiURL });
+const app = createApp(App);
+app.config.globalProperties.$axios = axiosInstance;
+app.config.globalProperties.$apiURL = apiURL;
 
-createApp(App).use(router).mount("#app");
+app.use(router).mount("#app");
