e7cd6ea4e1a730f1deb9369f0532617a15a51c0f
davidscottconrad <david.scott.conrad@gmail.com>
2024-04-01 15:00:52 -0400

    testing oruter again 4

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
-
+const app = createApp(App);
 const apiURL = process.env.VUE_APP_API_URL;
 const axiosInstance = axios.create({ baseURL: apiURL });
 
 app.config.globalProperties.$axios = axiosInstance;
 app.config.globalProperties.$apiURL = apiURL;
-const app = createApp(App);
 
 app.use(router); // Use the router in the app
 
